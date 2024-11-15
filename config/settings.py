c7fa442be1602864faafcf9951eb2741dfb32a9e
Nataliia <dominok56@gmail.com>
2024-11-15 20:24:20 +0200

    added app

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'softserve.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'softserve.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
